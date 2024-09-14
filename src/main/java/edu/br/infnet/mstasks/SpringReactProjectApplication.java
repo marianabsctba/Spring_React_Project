e00555724c1d64b8d79e7bf3dd764d1aa1de622e
Rwilliamsc <rwilliam.sc@gmail.com>
2024-09-14 02:56:12 -0300

    feat: adjust project

--- src/main/java/edu/br/infnet/mstasks/SpringReactProjectApplication.java
+++ src/main/java/edu/br/infnet/mstasks/SpringReactProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsTasksApplication {
+public class SpringReactProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MsTasksApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactProjectApplication.class, args);
+	}
 
 }
