0718e00db8b7e19eab6b420f8836bcd9c03f9cda
Rwilliamsc <rwilliam.sc@gmail.com>
2024-09-14 02:29:01 -0300

    feat: remove files

--- pb/src/main/java/pb/marianabs/infnet/edu/pb/exception/TaskNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package pb.marianabs.infnet.edu.pb.exception;
-
-public class TaskNotFoundException extends RuntimeException {
-    public TaskNotFoundException(Long id) {
-        super("Task not found with id: " + id);
-    }
-}
---
+++ src/main/java/edu/br/infnet/mstasks/MsTasksApplication.java
@@ -0,0 +1,13 @@
+package edu.br.infnet.mstasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsTasksApplication.class, args);
+    }
+
+}
